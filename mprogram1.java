21b4f05f6896cb1bb946879ca8907d725f8914d9
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-25 21:35:36 +0530

    Add files via upload

---
+++ mprogram1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class mprogram1
+{
+    public void main()
+    {
+        int a=10,b=0;
+        int sum;
+        sum = a+b;
+        System.out.println("the sum is =" + sum);
+    }
+}
